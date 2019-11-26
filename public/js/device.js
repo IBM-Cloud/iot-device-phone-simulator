89ec10a3da5e6c0f7bb55c35264ca39a398bc96b
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2019-11-26 16:52:57 +0530

    Added fix for iOS 13

--- public/js/device.js
+++ public/js/device.js
@@ -14,158 +14,193 @@
  *   Bryan Boyd - Initial implementation
  *******************************************************************************/
 (function(window) {
-	var ax = 0,
-	ay = 0,
-	az = 0,
-	oa = 0,
-	ob = 0,
-	og = 0;
+  var ax = 0,
+    ay = 0,
+    az = 0,
+    oa = 0,
+    ob = 0,
+    og = 0;
 
-	var client;
-	var iot_host;
-	var iot_port;
-	var iot_clientid;
-	var iot_username;
-	var iot_password;
-	var topic;
+  var client;
+  var iot_host;
+  var iot_port;
+  var iot_clientid;
+  var iot_username;
+  var iot_password;
+  var topic;
 
-	var isConnected = false;
+  var isConnected = false;
 
-	var last_sample = {};
-	var shifted_filter = {};
-	// High-pass filter to remove gravity offset from the acceleration waveforms
-	function filterOffset(sample, channel) {
-		if(sample === null || sample === undefined) return 0;
-		if(last_sample[channel] === undefined) last_sample[channel] = sample;
-		if(shifted_filter[channel] === undefined) shifted_filter[channel] = 0;
-		var shiftedFCL = shifted_filter[channel] + ((sample-last_sample[channel])*256);
-		shifted_filter[channel] = shiftedFCL - (shiftedFCL/256);
-		last_sample[channel] = sample;
-		return ((shifted_filter[channel]+128)/256);
-	}
+  var last_sample = {};
+  var shifted_filter = {};
+  // High-pass filter to remove gravity offset from the acceleration waveforms
+  function filterOffset(sample, channel) {
+    if (sample === null || sample === undefined) return 0;
+    if (last_sample[channel] === undefined) last_sample[channel] = sample;
+    if (shifted_filter[channel] === undefined) shifted_filter[channel] = 0;
+    var shiftedFCL =
+      shifted_filter[channel] + (sample - last_sample[channel]) * 256;
+    shifted_filter[channel] = shiftedFCL - shiftedFCL / 256;
+    last_sample[channel] = sample;
+    return (shifted_filter[channel] + 128) / 256;
+  }
 
-	window.ondevicemotion = function(event) {
-		ax = parseFloat((event.acceleration.x || filterOffset(event.accelerationIncludingGravity.x, "ax") || 0));
-		ay = parseFloat((event.acceleration.y || filterOffset(event.accelerationIncludingGravity.y, "ay") || 0));
-		az = parseFloat((event.acceleration.z || filterOffset(event.accelerationIncludingGravity.z, "az") || 0));
+  window.ondevicemotion = function(event) {
+    ax = parseFloat(
+      event.acceleration.x ||
+        filterOffset(event.accelerationIncludingGravity.x, "ax") ||
+        0
+    );
+    ay = parseFloat(
+      event.acceleration.y ||
+        filterOffset(event.accelerationIncludingGravity.y, "ay") ||
+        0
+    );
+    az = parseFloat(
+      event.acceleration.z ||
+        filterOffset(event.accelerationIncludingGravity.z, "az") ||
+        0
+    );
 
-		document.getElementById("accx").innerHTML = ax.toFixed(2);
-		document.getElementById("accy").innerHTML = ay.toFixed(2);
-		document.getElementById("accz").innerHTML = az.toFixed(2);
-	};
+    document.getElementById("accx").innerHTML = ax.toFixed(2);
+    document.getElementById("accy").innerHTML = ay.toFixed(2);
+    document.getElementById("accz").innerHTML = az.toFixed(2);
+  };
 
-	window.ondeviceorientation = function(event) {
-		oa = (event.alpha || 0);
-		ob = (event.beta || 0);
-		og = (event.gamma || 0);
+  window.ondeviceorientation = function(event) {
+    oa = event.alpha || 0;
+    ob = event.beta || 0;
+    og = event.gamma || 0;
 
-		if (event.webkitCompassHeading) {
-			oa = -event.webkitCompassHeading;
-		}
+    if (event.webkitCompassHeading) {
+      oa = -event.webkitCompassHeading;
+    }
 
-		document.getElementById("alpha").innerHTML = oa.toFixed(2);
-		document.getElementById("beta").innerHTML = ob.toFixed(2);
-		document.getElementById("gamma").innerHTML = og.toFixed(2);
-	};
+    document.getElementById("alpha").innerHTML = oa.toFixed(2);
+    document.getElementById("beta").innerHTML = ob.toFixed(2);
+    document.getElementById("gamma").innerHTML = og.toFixed(2);
+  };
 
-	window.msgCount = 0;
+  window.msgCount = 0;
 
-	function publish() {
-		// We only attempt to publish if we're actually connected, saving CPU and battery
-		if (isConnected) {
-			var payload = {
-				"d": {
-					"id": window.deviceId,
-					"ts": (new Date()).getTime(),
-					"ax": parseFloat(ax.toFixed(2)),
-					"ay": parseFloat(ay.toFixed(2)),
-					"az": parseFloat(az.toFixed(2)),
-					"oa": parseFloat(oa.toFixed(2)),
-					"ob": parseFloat(ob.toFixed(2)),
-					"og": parseFloat(og.toFixed(2))
-				}
-			};
-			var message = new Paho.MQTT.Message(JSON.stringify(payload));
-			message.destinationName = topic;
-			try {
-				window.client.send(message);
-				window.msgCount += 1;
-				$("#msgCount").html(window.msgCount);
-				console.log("[%s] Published", new Date().getTime());
-			} catch (err) {
-				console.error(err);
-				isConnected = false;
-				changeConnectionStatusImage("images/disconnected.svg");
-				document.getElementById("connection").innerHTML = "Disconnected";
-				setTimeout(connectDevice(), 1000);
-			}
-		}
-	}
+  function publish() {
+    // We only attempt to publish if we're actually connected, saving CPU and battery
+    if (isConnected) {
+      var payload = {
+        d: {
+          id: window.deviceId,
+          ts: new Date().getTime(),
+          ax: parseFloat(ax.toFixed(2)),
+          ay: parseFloat(ay.toFixed(2)),
+          az: parseFloat(az.toFixed(2)),
+          oa: parseFloat(oa.toFixed(2)),
+          ob: parseFloat(ob.toFixed(2)),
+          og: parseFloat(og.toFixed(2))
+        }
+      };
+      var message = new Paho.MQTT.Message(JSON.stringify(payload));
+      message.destinationName = topic;
+      try {
+        window.client.send(message);
+        window.msgCount += 1;
+        $("#msgCount").html(window.msgCount);
+        console.log("[%s] Published", new Date().getTime());
+      } catch (err) {
+        console.error(err);
+        isConnected = false;
+        changeConnectionStatusImage("images/disconnected.svg");
+        document.getElementById("connection").innerHTML = "Disconnected";
+        setTimeout(connectDevice(), 1000);
+      }
+    }
+  }
 
-	function onConnectSuccess() {
-		// The device connected successfully
-		console.log("Connected Successfully!");
-		isConnected = true;
-		changeConnectionStatusImage("images/connected.svg");
-		document.getElementById("status_message").innerHTML = "";
-		document.getElementById("connection").innerHTML = "Connected";
-	}
+  function onConnectSuccess() {
+    // The device connected successfully
+    console.log("Connected Successfully!");
+    isConnected = true;
+    changeConnectionStatusImage("images/connected.svg");
+    document.getElementById("status_message").innerHTML = "";
+    document.getElementById("connection").innerHTML = "Connected";
+  }
 
-	function onConnectFailure() {
-		// The device failed to connect. Let's try again in one second.
-		document.getElementById("status_message").innerHTML = "Could not connect to IBM Watson IoT Platform! Trying again in one second.";
-		console.log("Could not connect to IBM Watson IoT Platform! Trying again in one second.");
-		setTimeout(connectDevice(), 1000);
-	}
+  function onConnectFailure() {
+    // The device failed to connect. Let's try again in one second.
+    document.getElementById("status_message").innerHTML =
+      "Could not connect to IBM Watson IoT Platform! Trying again in one second.";
+    console.log(
+      "Could not connect to IBM Watson IoT Platform! Trying again in one second."
+    );
+    setTimeout(connectDevice(), 1000);
+  }
 
-	function connectDevice() {
-		topic = "iot-2/evt/sensorData/fmt/json";
-		console.log(window.deviceId, window.password);
-		$("#deviceId").html(window.deviceId);
+  function permission() {
+    // feature detect
+    if (typeof DeviceMotionEvent.requestPermission === "function") {
+      DeviceMotionEvent.requestPermission()
+        .then(permissionState => {
+          if (permissionState === "granted") {
+            window.addEventListener("devicemotion", () => {});
+          }
+        })
+        .catch(console.error);
+    } else {
+      // handle regular non iOS 13+ devices
+    }
+  }
+  function connectDevice() {
+    permission();
+    topic = "iot-2/evt/sensorData/fmt/json";
+    console.log(window.deviceId, window.password);
+    $("#deviceId").html(window.deviceId);
 
-		changeConnectionStatusImage("images/connecting.svg");
-		document.getElementById("connection").innerHTML = "Connecting";
-		console.log("Connecting device to IBM Watson IoT Platform...");
-		window.client.connect({
-			onSuccess: onConnectSuccess,
-			onFailure: onConnectFailure,
-			userName: "use-token-auth",
-			password: window.password,
-			useSSL: window.location.protocol == "https:"
-		});
-	}
+    changeConnectionStatusImage("images/connecting.svg");
+    document.getElementById("connection").innerHTML = "Connecting";
+    console.log("Connecting device to IBM Watson IoT Platform...");
+    window.client.connect({
+      onSuccess: onConnectSuccess,
+      onFailure: onConnectFailure,
+      userName: "use-token-auth",
+      password: window.password,
+      useSSL: window.location.protocol == "https:"
+    });
+  }
 
-	function getCredentials() {
+  function getCredentials() {}
 
-	}
+  function registerDevice() {
+    console.log("Attempting connect");
+    connectDevice();
+    setInterval(publish, 1000);
+  }
 
-	function registerDevice() {
-		console.log("Attempting connect");
-		connectDevice();
-		setInterval(publish, 1000);
-	}
+  function changeConnectionStatusImage(image) {
+    // document.getElementById("connectionImage").src = image;
+  }
 
-	function changeConnectionStatusImage(image) {
-		// document.getElementById("connectionImage").src = image;
-	}
+  function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+      results = regex.exec(location.search);
+    return results === null
+      ? ""
+      : decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
 
-	function getParameterByName(name) {
-		name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-		var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-		results = regex.exec(location.search);
-		return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-	}
-
-	$("#connect").on("click", function() {
-		var org = $("input[name=org]").val();
-		window.iot_host = org + ".messaging.internetofthings.ibmcloud.com";
-		window.iot_port = window.location.protocol == "https:" ? 8883 : 1883;
-		window.devicetype = $("input[name=devicetype]").val();
-		window.deviceId = $("input[name=device]").val();
-		window.password = $("input[name=token]").val();
-		window.iot_clientid = "d:"+org+":" + window.devicetype  + ":" + window.deviceId;
-		window.client = new Paho.MQTT.Client(window.iot_host, window.iot_port, window.iot_clientid);
-		registerDevice();
+  $("#connect").on("click", function() {
+    var org = $("input[name=org]").val();
+    window.iot_host = org + ".messaging.internetofthings.ibmcloud.com";
+    window.iot_port = window.location.protocol == "https:" ? 8883 : 1883;
+    window.devicetype = $("input[name=devicetype]").val();
+    window.deviceId = $("input[name=device]").val();
+    window.password = $("input[name=token]").val();
+    window.iot_clientid =
+      "d:" + org + ":" + window.devicetype + ":" + window.deviceId;
+    window.client = new Paho.MQTT.Client(
+      window.iot_host,
+      window.iot_port,
+      window.iot_clientid
+    );
+    registerDevice();
   });
-
-}(window));
+})(window);
